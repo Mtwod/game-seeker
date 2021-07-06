954da57333c2977579c7d035b1c46a219c02db34
Mtwod <maximewong@hotmail.com>
2021-07-06 16:51:36 +0200

    Change game card from div to list

--- src/components/GameCardList/index.jsx
+++ src/components/GameCardList/index.jsx
@@ -2,7 +2,7 @@ import './style.scss';
 import GameCard from 'components/GameCard';
 
 const GameCardList = () => (
-  <div className="GameCardList">
+  <ul className="GameCardList">
     <GameCard />
     <GameCard />
     <GameCard />
@@ -10,7 +10,7 @@ const GameCardList = () => (
     <GameCard />
     <GameCard />
     <GameCard />
-  </div>
+  </ul>
 );
 
 export default GameCardList;
