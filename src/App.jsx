1f07e683ba376573215b8c0e11388c18ad73172e
Mtwod <maximewong@hotmail.com>
2021-07-25 19:02:36 +0200

    Change url basename for gh page

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   const displayedColorTheme = useRecoilValue(colorTheme);
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/game-seeker">
       <div className={`App ${displayedColorTheme}`}>
         <Switch>
           <Route path="/">
